8beb4caf84267814160557753da03ef3ccf16b39
brendan-rice-red-hawk <brice@redhawk-tech.com>
2025-08-24 09:21:33 -0400

    do stuff

---
+++ .storybook/preview.ts
@@ -0,0 +1,40 @@
+import type { Preview } from '@storybook/react-vite'
+import '@ionic/react/css/core.css';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+import { setupIonicReact } from '@ionic/react';
+
+setupIonicReact();
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: 'ionic',
+      values: [
+        {
+          name: 'ionic',
+          value: '#ffffff',
+        },
+        {
+          name: 'dark',
+          value: '#121212',
+        },
+      ],
+    },
+  },
+};
+
+export default preview;
